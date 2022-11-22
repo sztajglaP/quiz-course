6b4f186a3ee75c198ded0db323bcd04338718b3f
Piotr Sztajgla <sztajglaP@interia.eu>
2022-11-22 16:06:25 +0100

    second commit

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ class Quiz {
         this.countDownInfo = document.querySelector('#count-down');
         this.questionHeading = document.querySelector('#question-heading');
 
-        this.answersList = document.querySelector('#answers-list');
+        this.answersList = document.querySelector('.answers-list');
         this.summary = document.querySelector('.summary');
 
         this.submitButton = document.querySelector('#submit-answer');
@@ -78,7 +78,33 @@ class Quiz {
     }
 
     setNextQuestionData = () => {
+        this.currentQuestionIndex++;
 
+        if(this.currentQuestionIndex >= this.questions.length) {
+            console.log('Koniec quizu');
+            this.showSummary();
+            return;
+        }
+
+        const question = this.questions[this.currentQuestionIndex];
+        this.questionHeading.innerHTML = question.q;
+
+        const progressStr = `Pytanie ${this.currentQuestionIndex + 1} z ${this.questions.length}`;
+        this.progress.innerHTML = progressStr;
+
+        const answersHtml = question.answers.map((answerText, index) => {
+            const answerId = 'answer' + index;
+            return `
+                <li>
+                    <input type="radio" name="answer" id="${answerId}" data-index="${index}" class="answer">
+                    <label for="${answerId}">
+                        ${answerText}
+                    </label>
+                </li>
+            `;
+        }).join("");
+
+        this.answersList.innerHTML = answersHtml;
     }
 
     showSummary = () => {
